b098d7a2969fa18b43706f4184bcbebf12ebca1e
natansevero <natanseverolp@gmail.com>
2018-10-02 20:55:14 -0300

    Funcionamento do describe, context e it

---
+++ tests/main.spec.js
@@ -0,0 +1,21 @@
+/* eslint-env mocha */
+
+describe('Main', () => {
+    describe('Method A', () => {
+        context('Case 1', () => {
+            it('should happen blabla', () => {
+                // espera o que aconteça
+                // Entra de dados / método sum(2,2)
+                // Espera retornar (4) => true | (3) => false => broken test
+            });
+        });
+
+        context('Case 2', () => {
+
+        });
+    });
+
+    describe('Method B', () => {
+
+    });
+});
