8af92ef06cbcddb4c6c88554cb4aeb4a9d9ab863
natansevero <natanseverolp@gmail.com>
2018-10-02 21:19:47 -0300

    Comandos skip, only e bail

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -3,15 +3,27 @@
 describe('Main', () => {
     describe('Method A', () => {
         context('Case 1', () => {
-            it('should happen blabla', () => {
+            it.skip('should happen blabla', () => {
                 // espera o que aconteça
                 // Entra de dados / método sum(2,2)
                 // Espera retornar (4) => true | (3) => false => broken test
+                throw new Error('just an error');
             });
         });
 
         context('Case 2', () => {
+            it('should happen blabla', () => {
+                // espera o que aconteça
+                // Entra de dados / método sum(2,2)
+                // Espera retornar (4) => true | (3) => false => broken test
+                throw new Error('just an error');
+            });
 
+            it('should happen mimimi', () => {
+                // espera o que aconteça
+                // Entra de dados / método sum(2,2)
+                // Espera retornar (4) => true | (3) => false => broken test
+            });
         });
     });
 
