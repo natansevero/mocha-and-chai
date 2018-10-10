8b0cd18f1cacbfb37306ca1795f463763403d51f
natansevero <natanseverolp@gmail.com>
2018-10-09 21:11:42 -0300

    Iniciando calculadora. Fazendo os smoke testes

---
+++ src/main.js
@@ -0,0 +1,6 @@
+module.exports = {
+    sum: () => { },
+    sub: () => { },
+    div: () => { },
+    mult: () => { },
+};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,83 +1,33 @@
 /* eslint-env mocha */
 
-// describe('Main', () => {
-//     describe('Method A', () => {
-//         context('Case 1', () => {
-//             it('should happen blabla', () => {
-//                 // espera o que aconteça
-//                 // Entra de dados / método sum(2,2)
-//                 // Espera retornar (4) => true | (3) => false => broken test
-//                 throw new Error('just an error');
-//             });
-//         });
-
-//         context('Case 2', () => {
-//             it('should happen blabla', () => {
-//                 // espera o que aconteça
-//                 // Entra de dados / método sum(2,2)
-//                 // Espera retornar (4) => true | (3) => false => broken test
-//                 throw new Error('just an error');
-//             });
-
-//             it('should happen mimimi', () => {
-//                 // espera o que aconteça
-//                 // Entra de dados / método sum(2,2)
-//                 // Espera retornar (4) => true | (3) => false => broken test
-//             });
-//         });
-//     });
-
-//     describe('Method B', () => {
-
-//     });
-// });
 const { expect } = require('chai');
+const calc = require('../src/main.js');
 
 describe('Main', () => {
-    let arr;
+    // smoke test
+    describe('Smoke tests', () => {
+        it('should exist the calc lib', () => {
+            expect(calc).to.exist;
+        });
 
-    before(() => {
+        it('should exist the method `sum`', () => {
+            expect(calc.sum).to.exist;
+            expect(calc.sum).to.be.a('function');
+        });
 
-    });
+        it('should exist the method `sub`', () => {
+            expect(calc.sub).to.exist;
+            expect(calc.sub).to.be.a('function');
+        });
 
-    after(() => {
+        it('should exist the method `mult`', () => {
+            expect(calc.mult).to.exist;
+            expect(calc.mult).to.be.a('function');
+        });
 
-    });
-
-    beforeEach(() => {
-        arr = [1, 2, 3];
-    });
-
-    afterEach(() => {
-
-    });
-
-    // Testar tipos ou se existe (Smoke test)
-    it('should be an array', () => {
-        expect(arr).to.be.a('array');
-    });
-
-    it('should gave a size of 4 when push another value to array', () => {
-        arr.push(4);
-        expect(arr).to.have.length(4);
-    });
-
-    it('should remove the value 3 when use pop in the array', () => {
-        arr.pop();
-        expect(arr).to.not.include(3);
-    });
-
-    it('should have a size 2 when pop a value from the array', () => {
-        arr.pop();
-        expect(arr).to.have.length(2);
+        it('should exist the method `div`', () => {
+            expect(calc.div).to.exist;
+            expect(calc.div).to.be.a('function');
+        });
     });
 });
-
-// before
-// beforeEach
-// test1
-// afterEach
-// beforeEach
-// test2
-// afterEach
-// after
