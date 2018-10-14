8ea27c68eede01e3fb3e75add0c15d4a4cbbc303
natansevero <natanseverolp@gmail.com>
2018-10-13 22:17:58 -0300

    Usando o nyc para test coverage

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -34,4 +34,8 @@ describe('FizzBuzz', () => {
     it('should return the number when non-multiple', () => {
         expect(FizzBuzz(2)).to.be.equal(2);
     });
+
+    it('should return 0 when 0', () => {
+        expect(FizzBuzz(0)).to.be.equal(0);
+    });
 });
